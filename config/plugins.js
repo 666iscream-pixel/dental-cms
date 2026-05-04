475a8c22a216be65dc52c470e4fd79cfbef34758
666iscream-pixel <270764631+666iscream-pixel@users.noreply.github.com>
2026-05-04 10:24:48 -0400

    feat: add Cloudinary upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -9,4 +9,19 @@ module.exports = () => ({
   'drag-drop-content-types': {
     enabled: true,
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
